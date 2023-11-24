509145f303294ee8aa52bb0a3373beda1a9162fa
angye212001 <angye212001@yahoo.com>
2023-11-24 13:50:29 -0600

    upload

---
+++ PyBank/bankbudget.py
@@ -0,0 +1,36 @@
+import os
+import csv
+
+csvpath = os.path.join("..", "resources", "budget_data.csv")
+
+with open(csvpath, "r") as file_handler:
+    lines = file_handler.read()
+        print(lines)
+        print(type(lines))
+
+Header
+
+print("Financial Analysis")
+    
+
+The total number of months included in the dataset
+
+Date = "months"
+len(Date)
+print(len(Date))
+
+The net total amount of profit or loss over the entire period
+
+Total = "amount"
+for Total in range():
+print(Total)
+
+The changes in profit or loss over the entire period,and then the average of those changes
+
+
+The greatest increase in profits date and amount over the entire period
+
+x = (>1860000)
+The greatest decrease in profits date and amount over the entire period
+
+y = (<-1820000)
